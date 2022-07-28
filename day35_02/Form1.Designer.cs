11b1ee25076917c18aeb1741f104c7c2375fce1d
KANGDONGHOON0413 <kmerits@gmail.com>
2022-07-29 00:04:36 +0900

    초기 커밋.

---
+++ day35_02/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace day35_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.INPUT_Name = new System.Windows.Forms.TextBox();
+            this.BTN_OK = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(127, 121);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(465, 188);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "이름";
+            // 
+            // INPUT_Name
+            // 
+            this.INPUT_Name.Location = new System.Drawing.Point(172, 88);
+            this.INPUT_Name.Name = "INPUT_Name";
+            this.INPUT_Name.Size = new System.Drawing.Size(125, 27);
+            this.INPUT_Name.TabIndex = 2;
+            // 
+            // BTN_OK
+            // 
+            this.BTN_OK.Location = new System.Drawing.Point(304, 85);
+            this.BTN_OK.Name = "BTN_OK";
+            this.BTN_OK.Size = new System.Drawing.Size(94, 29);
+            this.BTN_OK.TabIndex = 3;
+            this.BTN_OK.Text = "찾기";
+            this.BTN_OK.UseVisualStyleBackColor = true;
+            this.BTN_OK.Click += new System.EventHandler(this.BTN_OK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BTN_OK);
+            this.Controls.Add(this.INPUT_Name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox INPUT_Name;
+        private System.Windows.Forms.Button BTN_OK;
+    }
+}
+
---
+++ day35_02/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace day35_02
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter SDA;
+        DataSet DSet;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new SqlConnection("server=.\\SQLEXPRESS;database=test; user id=sa; pwd=alencia;");
+            SDA = new SqlDataAdapter();
+            DSet = new DataSet();
+        }
+
+        private void BTN_OK_Click(object sender, EventArgs e)
+        {
+            string strSelect = "SELECT * FROM testTable01 WHERE name = @name";
+            SDA.SelectCommand = new SqlCommand(strSelect, conn);
+            SDA.SelectCommand.Parameters.Add("@name", SqlDbType.NVarChar);
+            SDA.SelectCommand.Parameters["@name"].Value = INPUT_Name.Text;
+            DSet.Clear();
+            if (SDA.Fill(DSet, "testTable01") != 0)
+            {
+                dataGridView1.DataSource = DSet.Tables["testTable01"];
+            }
+            else MessageBox.Show("찾는 데이터가 없습니다", "오류");
+        }
+    }
+}
